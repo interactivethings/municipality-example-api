2ed598370a55a76e2adfdc2326f5139e59f1ab68
Jeremy Stucki <jeremy@interactivethings.com>
2021-02-01 17:13:11 +0100

    Add queries

--- api/municipalities.js
+++ api/municipalities.js
@@ -1,7 +1,42 @@
+import {
+  Source
+} from "rdf-cube-view-query";
+
+const getMunicipalies = async ({ name, limit, source }) => {
+  const iri = `https://register.ld.admin.ch/municipality/${id}`;
+
+  const sparql = `{
+    SELECT DISTINCT ("municipality" AS ?type) (?municipality AS ?iri) (?municipalityLabel AS ?name) WHERE {
+      GRAPH <https://lindas.admin.ch/fso/agvch> {
+        VALUES ?class { <https://schema.ld.admin.ch/Municipality> <https://schema.ld.admin.ch/AbolishedMunicipality> }
+        ?municipality a ?class .
+        ?municipality <http://schema.org/name> ?municipalityLabel .
+      }
+      FILTER (regex(?municipalityLabel, ".*${name}.*", "i"))
+    } LIMIT ${limit}
+  }`;
+
+  const results = await source.client.query.select(sparql);
+
+  return results.length > 0
+    ? {
+        municipalities: results.map((r) => {
+          return { id, name: r.name.value };
+        }),
+      }
+    : null;
+};
+
 module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
+  const source = new Source({
+    endpointUrl:
+      process.env.SPARQL_ENDPOINT ?? "https://test.lindas.admin.ch/query",
+    // user: '',
+    // password: ''
   });
+
+  const municipalities = await getMunicipalies({name: req.query.name, limit: 100, source});
+
+
+  res.json(municipalities);
 };
---
+++ api/municipality.js
@@ -0,0 +1,35 @@
+import {
+  Source
+} from "rdf-cube-view-query";
+
+const getMunicipality = async ({ id, source }) => {
+  const iri = `https://register.ld.admin.ch/municipality/${id}`;
+
+  const sparql = `
+SELECT DISTINCT ?name {
+  <${iri}> <http://schema.org/name> ?name.
+}
+  `;
+
+  const result = (await source.client.query.select(sparql))[0];
+
+  return result ? { id, name: result.name.value } : null;
+};
+
+
+module.exports = (req, res) => {
+
+  const source = new Source({
+    endpointUrl:
+      process.env.SPARQL_ENDPOINT ?? "https://test.lindas.admin.ch/query",
+    // user: '',
+    // password: ''
+  });
+
+
+  const municipality = await getMunicipality({id: req.query.id, source});
+
+
+
+  res.json(municipality);
+};
